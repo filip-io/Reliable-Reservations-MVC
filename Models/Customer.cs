c2c6cf6b0b59526814d4a2242e0ffbf31a2a4a2e
filip-io <filip.n@outlook.com>
2024-09-11 10:17:56 +0200

    Add project files.

---
+++ Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Reliable_Reservations_MVC.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public  string PhoneNumber { get; set; }
+
+        public string Email { get; set; }
+    }
+}
